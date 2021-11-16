387c46e0ab13689e2da237a34e13dc7d68670e2d
Peter Brady <pbrady@mathworks.com>
2021-11-17 08:14:34 +1100

    Completed Training Run Completed the training run and added copyright notices.

--- getProjectRootDir.m
+++ getProjectRootDir.m
@@ -1,6 +1,6 @@
 function myRootDir = getProjectRootDir()
 %GETPROJECTROOTFOLDER Conveniance function to return root folder
-
+% Copyright 2021 The MathWorks, Inc.
 myProject = currentProject;
 myRootDir = myProject.RootFolder;
 
